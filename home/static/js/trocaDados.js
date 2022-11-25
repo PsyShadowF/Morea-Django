b23064fe2b4cf2c5c637f8ba9084f765ced9ee8b
hernique33comiitei <mulekinb@gmail.com>
2022-11-25 14:24:33 -0300

    add toogle dados

--- home/static/js/trocaDados.js
+++ home/static/js/trocaDados.js
@@ -1,7 +1,13 @@
+const tableAgua = document.querySelector('.tableAgua')
+const tableEnergia = document.querySelector('.tableEnergia')
+const tableGas = document.querySelector('.tableGas')
+
 // TROCAR OS DADOS
 
 const btnTrocarDados = document.getElementById('btnTrocarDados')
 let contTrocarDados = 1
+
+trocarDados()
 
 function trocarDados() {
     if (contTrocarDados == 3) {
@@ -12,10 +18,31 @@ function trocarDados() {
 
     if(contTrocarDados == 1) {
         btnTrocarDados.src = '/static/icons/light-icon-general.png'
+        try {
+            tableAgua.style.display = 'none'
+            tableGas.style.display = 'none'
+            tableEnergia.style.display = 'inline-table'
+        } catch (error) {
+          console.log(error)  
+        }
     } else if (contTrocarDados == 2) {
         btnTrocarDados.src = '/static/icons/water-icon-general.png'
+        try {
+            tableAgua.style.display = 'inline-table'
+            tableGas.style.display = 'none'
+            tableEnergia.style.display = 'none'
+        } catch (error) {
+            console.log(error)
+        }
     } else if (contTrocarDados == 3) {
         btnTrocarDados.src = '/static/icons/gas-icon-general.png'
+        try {
+            tableAgua.style.display = 'none'
+            tableGas.style.display = 'inline-table'
+            tableEnergia.style.display = 'none' 
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 
